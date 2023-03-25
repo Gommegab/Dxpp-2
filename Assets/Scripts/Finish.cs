1639bee164d0e8eb3f38489074e93eb44980affb
A. Roig <antiaroig@gmail.com>
2023-03-25 13:21:20 +0100

    Creación Trigger de meta

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            // TODO: Chamada a GameManager para 
+            Debug.Log("WIN");
+        }
+    }
+    
+}
