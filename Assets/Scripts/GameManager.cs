1f045ab7185a2946f4f096716ce64fc37a31061c
SoniaPria <webusable@protonmail.com>
2023-03-26 13:05:35 +0200

    GameManager con cuenta atrás, GameEnd y StageEnd

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public float gameDuration;
+
+    float timeRemaining;
+    bool gameOver, stageOver;
+
+    public bool GameOver { get { return gameOver; } }
+
+    void Awake() {
+        instance = this;
+    }
+
+    void Start()
+    {
+        gameOver = false;
+        stageOver = false;
+
+        gameDuration = 15f;
+        // Inicializar o cronómetro ca duración máxima do xogo
+        timeRemaining = gameDuration;
+    }
+
+    void Update()
+    {
+        if( ! stageOver )
+        {
+            // Restar o tempo transcurrido dende o último frame ao tempo restante
+            timeRemaining -= Time.deltaTime;
+
+            if ( timeRemaining <= 0f )
+            {
+                // Cando o crono chegue a cero, remata o xogo
+                GameEnd();
+            }
+
+            else {
+                // Mostrar el tiempo restante por consola
+                Debug.Log( "Chrono: " + ConvertSecondsToMinutesAndSeconds(timeRemaining) );
+            }
+        }
+    }
+
+    string ConvertSecondsToMinutesAndSeconds(float seconds)
+    {
+        int minutes = Mathf.FloorToInt(seconds / 60f);
+        int remainingSeconds = Mathf.RoundToInt(seconds % 60f);
+
+        // Formatear a cadea de saída
+        return string.Format("{0:00}:{1:00}", minutes, remainingSeconds);
+    }
+
+
+    public void StageEnd()
+    {
+        Debug.Log("WIN");
+        stageOver = true;
+    }
+
+    void GameEnd()
+    {
+        Debug.Log("GAME OVER");
+        gameOver = true;
+    }
+
+
+
+    // void OnGUI()
+    // {
+    //     GUIStyle style = GUI.skin.GetStyle ("label");
+    //     style.fontSize = 50;
+    //
+    //     // GUI.color = Color.white;
+    //     GUI.Label( new Rect(10, 10, 10, 20), "Chrono: " + timeRemaining );
+    // }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -27,39 +27,39 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        // Vector2 tmpPosition = transform.position;
-        // tmpPosition += velocity * Time.deltaTime;
-        // transform.position = tmpPosition;
-        bool isGrounded = IsGrounded();
-
-        // Values -1.0f, 0f, 1.0f
-        horizontal = Input.GetAxisRaw("Horizontal");
-
-        // Se activa la animación de correr cuando Player no esté parado
-        animator.SetBool( "running", horizontal != 0.0f );
-
-        // Se activa la animación de caer a velocidad Y hacia abajo
-        animator.SetBool( "falling", rb.velocity.y < 0 && !isGrounded );
-
-        if (isGrounded)
+        if ( ! GameManager.instance.GameOver )
         {
-            // Player mira en la misma dirección que su mvto.
-            if ( horizontal < 0.0f ) {
-                transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
-            }
-            else if ( horizontal > 0.0f ) {
-                transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-            }
+            bool isGrounded = IsGrounded();
 
-            if( Input.GetKeyDown(KeyCode.W) )
-            {
-                Jump();
-            }
+            // Values -1.0f, 0f, 1.0f
+            horizontal = Input.GetAxisRaw("Horizontal");
 
-            // Si no se pulsa la tecla de salto, se anula la animación
-            else if ( animator.GetBool("jumping") )
+            // Se activa la animación de correr cuando Player no esté parado
+            animator.SetBool( "running", horizontal != 0.0f );
+
+            // Se activa la animación de caer a velocidad Y hacia abajo
+            animator.SetBool( "falling", rb.velocity.y < 0 && !isGrounded );
+
+            if (isGrounded)
             {
-                animator.SetBool( "jumping", false );
+                // Player mira en la misma dirección que su mvto.
+                if ( horizontal < 0.0f ) {
+                    transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+                }
+                else if ( horizontal > 0.0f ) {
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                }
+
+                if( Input.GetKeyDown(KeyCode.W) )
+                {
+                    Jump();
+                }
+
+                // Si no se pulsa la tecla de salto, se anula la animación
+                else if ( animator.GetBool("jumping") )
+                {
+                    animator.SetBool( "jumping", false );
+                }
             }
         }
     }
@@ -87,7 +87,7 @@ public class Player : MonoBehaviour
         origin.y -= 0.65f;
         Vector3 direction = Vector3.down;
         float maxDistance = 0.1f;
-        
+
         LayerMask mask = LayerMask.GetMask("Ground");
         RaycastHit2D rc = Physics2D.Raycast(origin, direction, maxDistance, mask);
         Debug.DrawRay(origin, direction * maxDistance, Color.red );
