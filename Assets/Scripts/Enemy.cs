524d24512ab962c5d92060209f28748ba1685da6
SoniaPria <webusable@protonmail.com>
2023-03-31 14:34:43 +0200

    Animación de ataque e detección da Player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public GameObject player;
+    public Collider2D detectionCollider;
+    public Collider2D guardCollider;
+
     public float speed;
 
     int horizontal;
+
     Vector3 velocity;
     Animator animator;
 
 
     void Start()
     {
+        // Dirección do mov [ -1 | +1]
         horizontal = 1;
+
         animator = GetComponent<Animator>();
         velocity = new Vector3(speed, 0, 0) * horizontal;
     }
@@ -30,6 +37,10 @@ public class Enemy : MonoBehaviour
         Vector3 localScale = transform.localScale;
         localScale.x = horizontal;
         transform.localScale = localScale;
+
+        // Ignorando a colisión entre
+        // o collider de detección da Player e os Colliders de garda
+        Physics2D.IgnoreCollision( detectionCollider, guardCollider );
     }
 
     void ReverseMovement()
@@ -38,12 +49,48 @@ public class Enemy : MonoBehaviour
         velocity = new Vector3(speed, 0, 0) * horizontal;
     }
 
+    void LookAtPlayer()
+    {
+        Vector3 direction = player.transform.position - transform.position;
+
+        if( direction.x < 0.0f )
+        {
+            ReverseMovement();
+        }
+
+        // if( direction.x >= 0.0f )
+        // {
+        //     transform.localScale = new Vector3( 1.0f, 1.0f, 1.0f );
+        // }
+        //
+        // else {
+        //     transform.localScale = new Vector3( -1.0f, 1.0f, 1.0f );
+        // }
+    }
+
     void OnTriggerEnter2D( Collider2D other )
     {
         if( other.gameObject.CompareTag("EnemyGuard") )
         {
-            // print("Enemy.OnTriggerEnter2D");
+            print($"Enemy.OnTriggerEnter2D {other.name}");
             ReverseMovement();
+        }
+
+        if( other.gameObject.CompareTag("Player") )
+        {
+            print($"Enemy.OnTriggerEnter2D {other.name}");
+
+            animator.SetBool( "attack", true );
+        }
+    }
+
+    void OnTriggerExit2D( Collider2D other )
+    {
+        if( other.gameObject.CompareTag("Player") )
+        {
+            print($"Enemy.OnTriggerExit2D {other.name}");
+
+            animator.SetBool( "attack", false );
         }
     }
 }
