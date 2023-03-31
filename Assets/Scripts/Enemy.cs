f5df3342d64eeed21880446ad766c2af22746ac0
SoniaPria <webusable@protonmail.com>
2023-03-31 10:34:53 +0200

    Animación e movimento Enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+
+    int horizontal;
+    Vector3 velocity;
+    Animator animator;
+
+
+    void Start()
+    {
+        horizontal = 1;
+        animator = GetComponent<Animator>();
+        velocity = new Vector3(speed, 0, 0) * horizontal;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movimento
+        Vector3 position = transform.position;
+        position += velocity * Time.deltaTime;
+        transform.position = position;
+
+        Vector3 localScale = transform.localScale;
+        localScale.x = horizontal;
+        transform.localScale = localScale;
+    }
+}
